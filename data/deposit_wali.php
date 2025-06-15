ef2d9b7d14bc8448d11b90e26175b7de085aab8f
Andri Noviandy <andrinoviandy88@gmail.com>
2025-06-15 11:49:28 +0700

    deposit wali

--- data/deposit_wali.php
+++ data/deposit_wali.php
@@ -2,32 +2,30 @@
 include("../config/koneksi.php");
 include("../include/API.php");
 session_start();
-// error_reporting(0);
+error_reporting(0);
 ?>
 <?php
 $start = $_GET['start'];
 
-if (!isset($_GET['tgl1']) && !isset($_GET['tgl2'])) {
-    $url = $API . "json/$_GET[page].php?start=$start&status=$_GET[status]";
-    $file = @file_get_contents($url);
-
-    if ($file === FALSE) {
-        echo "Gagal mengambil data dari API: $url";
-        exit;
+if (isset($_GET['cari'])) {
+    $search = str_replace(" ", "%20", $_GET['cari']);
+    if (!isset($_GET['tgl1']) && !isset($_GET['tgl2'])) {
+        $file = file_get_contents($API . "json/$_GET[page].php?start=$start&cari=" . $search . "&status=$_GET[status]");
+        $file2 = file_get_contents($API . "json/$_GET[page].php?cari=" . $search . "&status=$_GET[status]");
+    } else {
+        $file = file_get_contents($API . "json/$_GET[page].php?start=$start&cari=" . $search . "&tgl1=" . $_GET['tgl1'] . "&tgl2=" . $_GET['tgl2'] . "&status=$_GET[status]");
+        $file2 = file_get_contents($API . "json/$_GET[page].php?cari=" . $search . "&tgl1=" . $_GET['tgl1'] . "&tgl2=" . $_GET['tgl2'] . "&status=$_GET[status]");
     }
-    $file2 = file_get_contents($API . "json/$_GET[page].php?status=$_GET[status]");
 } else {
-    $url = $API . "json/$_GET[page].php?start=" . $start . "&tgl1=" . $_GET['tgl1'] . "&tgl2=" . $_GET['tgl2'] . "&status=$_GET[status]";
-    $file = @file_get_contents($url);
-
-    if ($file === FALSE) {
-        echo "Gagal mengambil data dari API: $url";
-        exit;
+    if (!isset($_GET['tgl1']) && !isset($_GET['tgl2'])) {
+        $file = file_get_contents($API . "json/$_GET[page].php?start=$start&status=$_GET[status]");
+        $file2 = file_get_contents($API . "json/$_GET[page].php?status=$_GET[status]");
+    } else {
+        $file = file_get_contents($API . "json/$_GET[page].php?start=" . $start . "&tgl1=" . $_GET['tgl1'] . "&tgl2=" . $_GET['tgl2'] . "&status=$_GET[status]");
+        $file2 = file_get_contents($API . "json/$_GET[page].php?tgl1=" . $_GET['tgl1'] . "&tgl2=" . $_GET['tgl2'] . "&status=$_GET[status]");
     }
-    $file2 = file_get_contents($API . "json/$_GET[page].php?tgl1=" . $_GET['tgl1'] . "&tgl2=" . $_GET['tgl2'] . "&status=$_GET[status]");
 }
 $json = json_decode($file, true);
-echo $json; die();
 $jml2 = $file2;
 
 ?>
