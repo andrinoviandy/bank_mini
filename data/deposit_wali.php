629175b77560d2a6205d3c5dd889803311187bfa
Andri Noviandy <andrinoviandy88@gmail.com>
2025-06-16 11:58:06 +0700

    depsoit wali

--- data/deposit_wali.php
+++ data/deposit_wali.php
@@ -49,41 +49,47 @@ $jml2 = $file2;
         </thead>
         <?php
         $jml = count($json);
-        for ($i = 0; $i < $jml; $i++) {
+        if ($jml !== 0) {
+            for ($i = 0; $i < $jml; $i++) {
         ?>
-            <tr>
-                <td align="center"><?php echo $start += 1; ?></td>
-                <td><?php echo date("d-m-Y", strtotime($json[$i]['created_at']));  ?></td>
-                <td><?php echo $json[$i]['nama'];  ?></td>
-                <td><?php echo "Rp" . number_format($json[$i]['jumlah'], 0, ',', '.');  ?></td>
-                <td><?php echo $json[$i]['bank'];  ?></td>
-                <td>
-                    <?php if ($json[$i]['status'] == 0) { ?>
-                        <badge class="badge btn-info">Baru, Belum Konfirmasi</badge>
-                    <?php } ?>
-                    <?php if ($json[$i]['status'] == 1) { ?>
-                        <badge class="badge btn-primary">Sudah Konfirmasi</badge>
-                        <br>
-                        <badge class="badge btn-info" style="cursor: pointer;" onclick="lihatBukti('<?php echo $json[$i]['bukti_transfer']; ?>')">Lihat Bukti Transfer</badge>
-                    <?php } ?>
-                    <?php if ($json[$i]['status'] == 2) { ?>
-                        <badge class="badge btn-primary">Selesai</badge>
-                        <br>
-                        <?php if ($json[$i]['bank'] == 'Manual') { ?>
-                            <badge class="badge btn-success">Manual, Tidak Ada Bukti Transfer</badge>
-                        <?php } else { ?>
+                <tr>
+                    <td align="center"><?php echo $start += 1; ?></td>
+                    <td><?php echo date("d-m-Y", strtotime($json[$i]['created_at']));  ?></td>
+                    <td><?php echo $json[$i]['nama'];  ?></td>
+                    <td><?php echo "Rp" . number_format($json[$i]['jumlah'], 0, ',', '.');  ?></td>
+                    <td><?php echo $json[$i]['bank'];  ?></td>
+                    <td>
+                        <?php if ($json[$i]['status'] == 0) { ?>
+                            <badge class="badge btn-info">Baru, Belum Konfirmasi</badge>
+                        <?php } ?>
+                        <?php if ($json[$i]['status'] == 1) { ?>
+                            <badge class="badge btn-primary">Sudah Konfirmasi</badge>
+                            <br>
                             <badge class="badge btn-info" style="cursor: pointer;" onclick="lihatBukti('<?php echo $json[$i]['bukti_transfer']; ?>')">Lihat Bukti Transfer</badge>
                         <?php } ?>
-                    <?php } ?>
-                </td>
-                <?php if (isset($_GET['status']) && $_GET['status'] == '0,1') { ?>
-                    <td>
-                        <!-- href="index.php?page=karyawan&id_hapus=<?php echo $json[$i]['idd']; ?>" onclick="return confirm('Anda Yakin Akan Menghapus Item Ini ?')" -->
-                        <button class="btn btn-sm btn-success" onclick="konfirmasi('<?php echo $json[$i]['idd']; ?>', '<?php echo number_format($json[$i]['jumlah'], 0, ',', '.'); ?>')"><span data-toggle="tooltip" title="Hapus" class="fa fa-check"></span> Konfirmasi</button>
-                        &nbsp;
-                        <!-- <a href="index.php?page=detail_pinjaman&id=<?php echo $json[$i]['idd']; ?>" class="btn btn-xs btn-info" data-toggle="tooltip" data-title="Detai"><span class="fa fa-folder-open"></span></a> -->
+                        <?php if ($json[$i]['status'] == 2) { ?>
+                            <badge class="badge btn-primary">Selesai</badge>
+                            <br>
+                            <?php if ($json[$i]['bank'] == 'Manual') { ?>
+                                <badge class="badge btn-success">Manual, Tidak Ada Bukti Transfer</badge>
+                            <?php } else { ?>
+                                <badge class="badge btn-info" style="cursor: pointer;" onclick="lihatBukti('<?php echo $json[$i]['bukti_transfer']; ?>')">Lihat Bukti Transfer</badge>
+                            <?php } ?>
+                        <?php } ?>
                     </td>
-                <?php } ?>
+                    <?php if (isset($_GET['status']) && $_GET['status'] == '0,1') { ?>
+                        <td>
+                            <!-- href="index.php?page=karyawan&id_hapus=<?php echo $json[$i]['idd']; ?>" onclick="return confirm('Anda Yakin Akan Menghapus Item Ini ?')" -->
+                            <button class="btn btn-sm btn-success" onclick="konfirmasi('<?php echo $json[$i]['idd']; ?>', '<?php echo number_format($json[$i]['jumlah'], 0, ',', '.'); ?>')"><span data-toggle="tooltip" title="Hapus" class="fa fa-check"></span> Konfirmasi</button>
+                            &nbsp;
+                            <!-- <a href="index.php?page=detail_pinjaman&id=<?php echo $json[$i]['idd']; ?>" class="btn btn-xs btn-info" data-toggle="tooltip" data-title="Detai"><span class="fa fa-folder-open"></span></a> -->
+                        </td>
+                    <?php } ?>
+                </tr>
+            <?php } ?>
+        <?php } else { ?>
+            <tr>
+                <td colspan="7">Tidak Ada Data</td>
             </tr>
         <?php } ?>
     </table>
